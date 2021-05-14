230341da6e8c515fb1f6c8ff0da771de623349c9
André Nascimento <andrevsn1@gmail.com>
2021-05-14 18:04:04 -0300

    add icons

---
+++ src/config/svgs.js
@@ -0,0 +1,55 @@
+import AddUserIcon from '../assets/icons/add-user.svg';
+import ArrowLeftIcon from '../assets/icons/arrow-left.svg';
+import ArrowNextIcon from '../assets/icons/arrow-next.svg';
+import BellIcon from '../assets/icons/bell.svg';
+import BookIcon from '../assets/icons/book.svg';
+import CalendarIcon from '../assets/icons/calendar.svg';
+import CamIcon from '../assets/icons/cam.svg';
+import CloseIcon from '../assets/icons/close.svg';
+import CommunityIcon from '../assets/icons/community.svg';
+import EditIcon from '../assets/icons/edit.svg';
+import HambMenuIcon from '../assets/icons/hamb-menu.svg';
+import HomeIcon from '../assets/icons/home.svg';
+import LocationIcon from '../assets/icons/location.svg';
+import LogoutIcon from '../assets/icons/logout.svg';
+import OkIcon from '../assets/icons/ok.svg';
+import OptionsIcons from '../assets/icons/options.svg';
+import SettingsIcon from '../assets/icons/settings.svg';
+import SpeakerIcon from '../assets/icons/speaker.svg';
+import TagIcon from '../assets/icons/tag.svg';
+import ThumbsDownIcon from '../assets/icons/thumbs-down.svg';
+import ThumbsUpIcon from '../assets/icons/thumbs-up.svg';
+import WarningIcon from '../assets/icons/warning.svg';
+import ColoredLogoIcon from '../assets/icons/logo-final-aprovado.svg';
+import FacebookIcon from '../assets/icons/facebook.svg';
+import WhatsappIcon from '../assets/icons/whatsapp.svg';
+import DunnoIcon from '../assets/icons/dunno.svg';
+
+export {
+  AddUserIcon,
+  ArrowLeftIcon,
+  ArrowNextIcon,
+  BellIcon,
+  BookIcon,
+  CalendarIcon,
+  CamIcon,
+  CloseIcon,
+  CommunityIcon,
+  EditIcon,
+  HambMenuIcon,
+  HomeIcon,
+  LocationIcon,
+  LogoutIcon,
+  OkIcon,
+  OptionsIcons,
+  SettingsIcon,
+  SpeakerIcon,
+  TagIcon,
+  ThumbsDownIcon,
+  ThumbsUpIcon,
+  WarningIcon,
+  ColoredLogoIcon,
+  FacebookIcon,
+  WhatsappIcon,
+  DunnoIcon,
+};
